656cf476dd735e43f7c0fb3594fef4a3cc7f9aab
Poet-Big-Bang <spartakmovisan@gmail.com>
2024-05-09 17:14:00 +0200

    integrate card menu and scroll script

--- Step_Project/js/script.js
+++ Step_Project/js/script.js
@@ -1,5 +1,24 @@
-<script>
+{ <script>
+document.addEventListener("DOMContentLoaded", function() {
   document.getElementById('myVideo').addEventListener('play', function(){
     document.getElementById('video-title').style.opacity = 0;
   });
-</script>
+});
+
+function scrollToElement(selector) {
+    var element = document.querySelector(selector);
+    element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+}
+</script> }
+
+
+// document.addEventListener("DOMContentLoaded", function() {
+//   document.getElementById('myVideo').addEventListener('play', function(){
+//     document.getElementById('video-title').style.opacity = 0;
+//   });
+// });
+
+// function scrollToElement(selector) {
+//     var element = document.querySelector(selector);
+//     element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+// }
