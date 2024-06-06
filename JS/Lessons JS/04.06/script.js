dcba6b5fbc080b3003be23f4953a94501e4295cd
Poet-Big-Bang <spartakmovisan@gmail.com>
2024-06-06 13:08:56 +0200

    Pak to predelam

---
+++ JS/Lessons JS/04.06/script.js
@@ -0,0 +1,20 @@
+let odstavec = document.querySelector("p");
+
+console.log(odstavec);
+
+console.log(odstavec.textContent)
+
+console.log(odstavec.innerHTML)
+
+let vsechniOdstavce = document.querySelectorAll("p")
+let treti = vsechniOdstavce[2]
+
+console.log(treti)
+
+document.querySelectorAll(".item")
+
+document.getElementsByClassName("item")
+
+document.getElementsById("item3")
+
+
